1ecf69e443072c9f423082f9f2405fb039ec4e73
MasterpieceKODZ <grail.masterpiece@gmail.com>
2023-03-03 01:25:38 +0100

    I designed the login screen for small device and i added email and password validation, I also added font awesome css

---
+++ myFunctions/validatePassword.ts
@@ -0,0 +1,70 @@
+// review password while user is typing
+export async function validatePasswordOnChange(e: any) {
+	const passwordCheckCont = document.getElementById("password-check");
+	const letterCheckOk = document.querySelector(".pw-letter-check-ok");
+	const letterCheckBad = document.querySelector(".pw-letter-check-bad");
+	const numCheckOk = document.querySelector(".pw-num-check-ok");
+	const numCheckBad = document.querySelector(".pw-num-check-bad");
+	const nonAlphanumCheckOk = document.querySelector(
+		".pw-non-alphanum-check-ok",
+	);
+	const nonAlphanumCheckBad = document.querySelector(
+		".pw-non-alphanum-check-bad",
+	);
+	passwordCheckCont?.classList.remove("hidden");
+	letterCheckBad?.classList.remove("hidden");
+	numCheckBad?.classList.remove("hidden");
+	nonAlphanumCheckBad?.classList.remove("hidden");
+	const inputValue: string = e.target.value.trim();
+	const letterRegex = new RegExp("[a-z]{6,}", "gi");
+	const numRegex = new RegExp("[0-9]+", "gi");
+	const nonAlphaNumRegex = new RegExp(/\W/, "gi");
+
+	const letterTest = letterRegex.test(inputValue);
+	const numTest = numRegex.test(inputValue);
+	const nonAlphaNumTest = nonAlphaNumRegex.test(inputValue);
+
+	if (letterTest) {
+		letterCheckBad?.classList.add("hidden");
+		letterCheckOk?.classList.remove("hidden");
+	}
+
+	if (numTest) {
+		numCheckBad?.classList.add("hidden");
+		numCheckOk?.classList.remove("hidden");
+	}
+
+	if (nonAlphaNumTest) {
+		nonAlphanumCheckBad?.classList.add("hidden");
+		nonAlphanumCheckOk?.classList.remove("hidden");
+	}
+
+	if (!inputValue) {
+		letterCheckBad?.classList.remove("hidden");
+		letterCheckOk?.classList.add("hidden");
+		numCheckBad?.classList.remove("hidden");
+		numCheckOk?.classList.add("hidden");
+		nonAlphanumCheckBad?.classList.remove("hidden");
+		nonAlphanumCheckOk?.classList.add("hidden");
+	}
+}
+
+// validate password before submitting login form
+export function validatePasswordOnSubmit() {
+	const passwordInput: any = document.getElementById("inp-password-login");
+	const passwordValue = passwordInput.value.trim();
+
+	const letterRegex = new RegExp("[a-z]{6,}", "gi");
+	const numRegex = new RegExp("[0-9]+", "gi");
+	const nonAlphaNumRegex = new RegExp(/\W/, "gi");
+
+	const pwLetterTest = letterRegex.test(passwordValue);
+	const numTest = numRegex.test(passwordValue);
+	const nonAlphaTest = nonAlphaNumRegex.test(passwordValue);
+
+	if (pwLetterTest && numTest && nonAlphaTest) {
+		return true;
+	}
+
+	return false;
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,75 +1,114 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { sendEmailVerification, signOut } from "firebase/auth";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useEffect } from "react";
-import { appAuth } from "@/firebase.config";
-import { useRouter } from "next/router";
+import BallSpinnerModal from "@/components/BallSpinners";
+import initiateLoginProcess from "@/myFunctions/startLogin";
+import { validatePasswordOnChange } from "@/myFunctions/validatePassword";
+import Link from "next/link";
 
+/* eslint-disable @next/next/no-img-element */
 const Login = () => {
-	const router = useRouter();
-
-	const loginFunction = () => {
-		const emailInp: any = document.getElementById("email_inp_login");
-		const passwordInp: any = document.getElementById("inp_password_login");
-
-		createUserWithEmailAndPassword(
-			appAuth,
-			emailInp?.value as string,
-			passwordInp?.value as string,
-		).then((userData) => {
-			const actionCodeSetting = {
-				url: "http://localhost:3000/quizroom",
-				handleCodeInApp: false,
-			};
-
-			if (userData.operationType == "signIn" && !userData.user.emailVerified) {
-				sendEmailVerification(userData.user, actionCodeSetting).then(() => {
-					console.log("verification email sent...");
-				});
-			} else {
-				router.push("/quizroom");
-			}
-		});
-	};
-
-	useEffect(() => {
-		const unsubscribe = appAuth.onAuthStateChanged((user) => {
-			console.log(appAuth.currentUser);
-
-			console.log(user?.emailVerified);
-		});
-
-		return () => {
-			unsubscribe();
-		};
-	}, []);
-
 	return (
-		<div>
-			Login
-			<input
-				type="email"
-				id="email_inp_login"
-				placeholder="email"
-			/>
-			<input
-				type="password"
-				id="inp_password_login"
-				placeholder="password"
-			/>
-			<button onClick={loginFunction}>LOGIN</button>
-			<button
-				onClick={() => {
-					signOut(appAuth)
-						.then(() => {
-							console.log("user signed out...");
-						})
-						.catch((err) => {
-							console.log("an error occured ", err);
-						});
-				}}>
-				Sign Out
-			</button>
+		<div className="login-root min-h-[100vh] overflow-y-auto relative bg-slate-200">
+			<div className="flex items-center justify-center py-3">
+				<img
+					src="/brand.png"
+					alt="brand quizzical"
+					className="img-login-brand w-[30%] h-auto object-cover"
+				/>
+			</div>
+			<div className="login-form-root">
+				<form
+					className="flex flex-col items-center"
+					onSubmit={(e) => {
+						e.preventDefault();
+						initiateLoginProcess();
+					}}>
+					<div className=" w-max mt-4">
+						<label
+							htmlFor="#inp-email-login"
+							className="block text-[17px] ">
+							Email
+						</label>
+						<input
+							type="email"
+							name="loginemail"
+							id="inp-email-login"
+							className=" block border-2 w-[80vw] rounded-lg ring-1 ring-slate-600 outline-accent"
+							autoComplete="email"
+							required
+						/>
+						<p
+							id="email-error-login"
+							className="mt-2 text-red-600 text-[12px] w-[80vw] hidden">
+							this email address is not valid please enter a valid address and
+							try again
+						</p>
+					</div>
+					<div className=" w-max mt-6">
+						<label
+							htmlFor="inp-password-login"
+							className="block text-[17px]">
+							Password
+						</label>
+						<input
+							type="password"
+							name="loginpassword"
+							id="inp-password-login"
+							className=" block border-2 w-[80vw] rounded-lg ring-1 ring-slate-600  outline-accent"
+							autoComplete="current-password"
+							onChange={(e) => {
+								validatePasswordOnChange(e);
+							}}
+							required
+						/>
+						<div
+							id="password-check"
+							className="text-[12px] hidden">
+							<p>at least...</p>
+							<p className="pw-letter-check-ok text-green-500 hidden">
+								6 letters. <i className="fas fa-check"></i>
+							</p>
+							<p className="pw-letter-check-bad text-red-500 hidden">
+								6 letters. <i className="fas fa-xmark"></i>
+							</p>
+							<p className="pw-num-check-ok text-green-500 hidden">
+								1 number. <i className="fas fa-check"></i>
+							</p>
+							<p className="pw-num-check-bad text-red-500 hidden">
+								1 number. <i className="fas fa-xmark"></i>
+							</p>
+							<p className="pw-non-alphanum-check-ok text-green-500 hidden">
+								1 non-alphanumeric character. <i className="fas fa-check"></i>
+							</p>
+							<p className="pw-non-alphanum-check-bad text-red-500 hidden">
+								1 non-alphanumeric character. <i className="fas fa-xmark"></i>
+							</p>
+						</div>
+						<p
+							id="password-error-login"
+							className="mt-2 text-red-600 text-[12px] w-[80vw] hidden">
+							incorrect password you have four more attempts
+						</p>
+					</div>
+
+					<button
+						type="submit"
+						className="px-8 py-[2px] mt-8 rounded-lg bg-primary ring-2 ring-secondary text-white text-[18px]">
+						Login
+					</button>
+					<p className="w-[80%] text-center text-red-600 bg-red-200 p-2 mt-5 rounded-md ring-1 ring-red-400 hidden">
+						There was an error while processing your data please check your
+						network and try again.
+					</p>
+				</form>
+			</div>
+			<p className="text-[18] w-full text-center absolute bottom-2">
+				Don`t have an account...?{" "}
+				<Link
+					href="/signup"
+					className=" text-blue-600">
+					Sign Up
+				</Link>
+			</p>
+			<BallSpinnerModal />
 		</div>
 	);
 };
