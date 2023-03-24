b1dc614fe9245ecf33b476163e21d95671f01597
MasterpieceKODZ <grail.masterpiece@gmail.com>
2023-03-24 20:43:18 +0100

    I fixed some typos

--- challenge.ts
+++ challenge.ts
@@ -1,14 +1,14 @@
 const challenge1 =
-	"How much do you know...? well, don`t just say it! prove it!\nTake a 20 question quiz on any subject of your intrest and prove yourself...!";
+	"How much do you know? well, don`t just say it! prove it!\nTake a 20 question quiz on any subject of your intrest and prove yourself!";
 
 const challenge2 =
-	"Do you think you are smart...? well, let see what you`ve got,\nTake a 20 question quiz on any subject of your choice, we shall see how much you really know...!";
+	"Do you think you are smart? well, let`s see what you`ve got,\nTake a 20 question quiz on any subject of your choice, and we shall see how much you really know!";
 
 const challenge3 =
-	"You think you are smart...? maybe or maybe not, there is only one way to find out,\nTake a 20 question quiz on any subject of your intrest, and we shall see...!";
+	"You think you are smart? maybe or maybe not, there is only one way to find out,\nTake a 20 question quiz on any subject of your intrest, and we shall see!";
 
 const challenge4 =
-	'Are you smart...? hmmm... there is a saying that "action speaks louder than words..." prove yourself by taking a quiz on any subject of your choice...!';
+	'Are you smart? hmmm... there is a saying that "action speaks louder than words". prove yourself by taking a 20 question quiz on any subject of your choice!';
 
 // return a random challenge text
 export function getChallengeText() {
